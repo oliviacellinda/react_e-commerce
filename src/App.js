c5ed0dfc1d440998c6020bbd546b72384cf81aab
oliviacellinda <oliviacellinda@gmail.com>
2020-07-01 14:21:29 +0700

    added sass and homepage file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+
+import Homepage from "./homepage.component";
+
 import "./App.css";
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
