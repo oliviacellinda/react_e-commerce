037c37990d30260a3510cad3b746e5f8e45ce9fa
oliviacellinda <oliviacellinda@gmail.com>
2020-07-03 00:53:13 +0700

    add note about Route, Switch, and HOC

--- src/App.js
+++ src/App.js
@@ -11,6 +11,31 @@ const HatsPage = () => (
   </div>
 );
 
+/**
+ * About Route
+ * When a component is set in Route, an object is passed as prop to the component (see below code).
+ * By checking the prop through console.log, we can see that there are 3 objects inside.
+ * 1. history (https://reacttraining.com/react-router/core/api/history) (https://drive.google.com/file/d/1fZPxYXhFMGC-4D8wOxvqkwvZjS4_4jqA/view?usp=sharing)
+ * 2. location (https://reacttraining.com/react-router/core/api/location) (https://drive.google.com/file/d/1QliOPmnNHWuPCiX5NGPnjaD8q3ZcPgWI/view?usp=sharing)
+ * 3. match (https://reacttraining.com/react-router/core/api/match) (https://drive.google.com/file/d/1Ya1t6RrxBQb8gBCBDc2vTONz7DrkuIbP/view?usp=sharing)
+ * Various function or property available inside those three will be used in navigating.
+ * 
+ * 
+ * Code fragment:
+    const HatsPage = (props) => {
+      console.log(props);
+      return (
+        <div>
+          <h1>Hats Page</h1>
+        </div>
+      );
+    };
+ * 
+ * 
+ * About Switch
+ * We can put many Routes inside Switch, but only first matched path will be used.
+ */
+
 function App() {
   return (
     <div>
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -22,3 +22,18 @@ const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
 );
 
 export default withRouter(MenuItem);
+
+/**
+ * The export expression above is using High-Order Component by putting MenuItem Component inside withRouter.
+ * This is done so we can use Route property from App.js.
+ *
+ * As shown in code fragment inside App.js, we can use Route object of the assigned component.
+ * Unfortunately, we can only use those object inside the said component and in order to use it inside child property,
+ * we need to pass it as child's property when we use the child component.
+ * In this example we need to use the Route object only in menu-item component, not in the directory component.
+ * It is considered as bad practice to pass unused property to the child component (prop drilling).
+ *
+ * To avoid that, we can use High-Order Component (HOC) as shown in the above expression.
+ * By doing this, we can now use React object that is passed from App.js without explicitly pass it as property
+ * in the directory component.
+ */
