0b556b332cadfe6a16c956bc84df385f472eeade
oliviacellinda <oliviacellinda@gmail.com>
2020-07-26 01:46:34 +0700

    updating app with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,21 +1,43 @@
 import React from "react";
 
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import "./cart-icon.style.scss";
 import { connect } from "react-redux";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ itemCount, toggleCartHidden }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
+
+const mapStateToProps = (state) => ({
+  /**
+   * reduce() is native Javascript method that works similarly like a for loop
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+   *
+   * 0 is the initial value which will be used in the callback function
+   * It means the first value of accumulatedQuantity is 0
+   * The callback function then return the calculation result
+   * which will be used as the next accumulatedQuantity value in the next loop
+   *
+   * We don't need this piece of code anymore because we replace it with memoized selector imported from cart.selectors.js
+   * We also replace the previous argument of this function: { cart: { cartItems } }
+   * itemCount: cartItems.reduce(
+   *   (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+   *     0
+   * ),
+   */
+
+  itemCount: selectCartItemsCount(state),
+});
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,47 @@
+import { createSelector } from "reselect";
+
+const selectCart = (state) => state.cart;
+
+/**
+ * createSelector(...inputSelectors | [inputSelectors], resultFunc)
+ * Takes one or more selectors, or an array of selectors,
+ * computes their values and passes them as arguments to resultFunc.
+ *
+ * createSelector determines if the value returned by an input-selector has changed between calls using reference equality (===).
+ * Inputs to selectors created with createSelector should be immutable.
+ *
+ * Selectors created with createSelector have a cache size of 1.
+ * This means they always recalculate when the value of an input-selector changes,
+ * as a selector only stores the preceding value of each input-selector.
+ */
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
+
+/**
+ * About Memoization
+ * Whenever a part of state tree is updated, every component using state value will re-render
+ * This will also occur even when the new state value is the same as its previous value
+ * When the state tree is large, this will cause performance problem
+ *
+ * https://www.npmjs.com/package/reselect
+ * Reselect provides a function createSelector for creating memoized selectors.
+ * createSelector takes an array of input-selectors and a transform function as its arguments.
+ * If the Redux state tree is mutated in a way that causes the value of an input-selector to change,
+ * the selector will call its transform function with the values of the input-selectors as arguments and return the result.
+ * If the values of the input-selectors are the same as the previous call to the selector,
+ * it will return the previously computed value instead of calling the transform function.
+ *
+ */
